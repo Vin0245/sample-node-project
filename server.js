e7ac8bff0374b04238850d10c0cec47c90228fa9
Vinlinuxtutors <108888462+Vin0245@users.noreply.github.com>
2024-06-06 22:53:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.get("/hearbeat", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://autocheckt:${PORT}`);
 });
