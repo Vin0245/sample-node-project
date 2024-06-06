002d702c9b55c456003b4da0cc311993b9b023cc
vin <vinayakojha@bitcot.com>
2024-06-06 16:51:09 +0530

    adding code

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+// const puppeteer = require("puppeteer");
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = 4000;
+
+app.get("/hearbeat", async (req, res) => {
+  return res.send({ message: "OK" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
