fc172e27c212657cf9ce594d515062d9957c45fe
Vinlinuxtutors <108888462+Vin0245@users.noreply.github.com>
2024-06-07 15:42:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.get("/hearbeat", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://check with rolling update completely with webhook test-3:${PORT}`);
+  console.log(`Server running at http://check with rolling update completely with webhook test-4:${PORT}`);
 });
