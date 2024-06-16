61e60f732ed883e8c3e7a6727a7893b1037ecc13
Vinlinuxtutors <108888462+Vin0245@users.noreply.github.com>
2024-06-16 16:48:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Define the route for the root path ("/")
 app.get('/', (req, res) => {
-    const sentence = 'Welcome to the CICD Automation with clodbyvin';
+    const sentence = 'Welcome to the CICD Automation with cloudbyvin checking';
 
     // Send the sentence as the response
     res.json({ sentence });
