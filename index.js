f8677b144e907c9f13c60f23259f9e50b5a4ad95
Ubuntu <ubuntu@ip-172-31-13-171.ap-south-1.compute.internal>
2024-09-28 04:58:54 +0000

    coursevita

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Define the route for the root path ("/")
 app.get('/', (req, res) => {
-    const sentence = 'Welcome to the CICD Automation with cloudbyvin checking success with cicd ';
+    const sentence = 'Welcome to the CICD Automation with coursevita checking success with cicd ';
 
     // Send the sentence as the response
     res.json({ sentence });
